c6bb283e5adfb07ff964d66ef42bcd14b04728e6
fburgos <f.burgos.valdes@gmail.com>
2018-09-28 18:01:36 +0200

    Add Boostrap 4

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,14 +1,17 @@
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { SharedModule } from '@app/shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
