cd86d0ab9937e4e2d147d83ea57af5ec1a184c5b
fburgos <f.burgos.valdes@gmail.com>
2019-01-26 01:37:58 +0100

    Fix truffle and travis config

--- dapp/truffle.js
+++ dapp/truffle.js
@@ -17,6 +17,12 @@ var HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 8000000
+    },
     ropsten: {
       provider: function() {
         return new HDWalletProvider([
