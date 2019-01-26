4e59f1a8fc447e1660a1143e3d776307360032c3
fburgos <f.burgos.valdes@gmail.com>
2019-01-26 01:24:17 +0100

    Update Angular version to 7.2.2

--- dapp/truffle.js
+++ dapp/truffle.js
@@ -16,27 +16,24 @@ var HDWalletProvider = require("truffle-hdwallet-provider");
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gas: 8000000
-    },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(process.env.METAMASK_MNEMONIC, process.env.INFURA_API_ROPSTEN_ENDPOINT)
+        return new HDWalletProvider([
+          process.env.ACCOUNT_PRIVATE_KEY_0,
+          process.env.ACCOUNT_PRIVATE_KEY_1,
+          process.env.ACCOUNT_PRIVATE_KEY_2,
+          process.env.ACCOUNT_PRIVATE_KEY_3,
+          process.env.ACCOUNT_PRIVATE_KEY_4,
+          process.env.ACCOUNT_PRIVATE_KEY_5,
+          process.env.ACCOUNT_PRIVATE_KEY_6,
+          process.env.ACCOUNT_PRIVATE_KEY_7,
+          process.env.ACCOUNT_PRIVATE_KEY_8,
+          process.env.ACCOUNT_PRIVATE_KEY_9,
+        ], process.env.INFURA_API_ROPSTEN_ENDPOINT, 0, 10)
       },
       network_id: 3,
       gas: 8000000
-    },
-    solc: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
     }
   }
 };
